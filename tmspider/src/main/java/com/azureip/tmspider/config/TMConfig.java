2329d18972b2236c8832c5b9dc497122f045708b
lewiszhang <fgwx@vip.qq.com>
2019-01-29 17:16:07 +0800

    async task scheduler added

--- tmspider/src/main/java/com/azureip/tmspider/config/TMConfig.java
+++ tmspider/src/main/java/com/azureip/tmspider/config/TMConfig.java
@@ -2,10 +2,14 @@ package com.azureip.tmspider.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -15,10 +19,10 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import java.util.concurrent.Executor;
 
 @Configuration
-@EnableAsync
 @EnableWebMvc
+@EnableAsync
 @EnableScheduling
-public class TMConfig implements WebMvcConfigurer, AsyncConfigurer {
+public class TMConfig implements WebMvcConfigurer, SchedulingConfigurer, AsyncConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -30,21 +34,10 @@ public class TMConfig implements WebMvcConfigurer, AsyncConfigurer {
         registry.addResourceHandler("/*").addResourceLocations("classpath:/static/");
     }
 
-    @Bean
-    public Executor tmAsyncExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(8);
-        executor.setMaxPoolSize(8);
-        executor.setQueueCapacity(16);
-        executor.setKeepAliveSeconds(30);
-        // 等待所有线程执行完
-        executor.setWaitForTasksToCompleteOnShutdown(true);
-        // 等待时间
-        executor.setAwaitTerminationSeconds(90);
-        // 线程名称前缀
-        executor.setThreadNamePrefix("TMSpider-");
-        executor.initialize();
-        return executor;
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        TaskScheduler scheduler = tmTaskScheduler();
+        taskRegistrar.setTaskScheduler(scheduler);
     }
 
     @Bean
@@ -53,6 +46,34 @@ public class TMConfig implements WebMvcConfigurer, AsyncConfigurer {
         viewResolver.setPrefix("/static/");
         viewResolver.setSuffix(".html");
         return viewResolver;
+    }
+
+    @Bean
+    public Executor tmAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(8);
+        executor.setQueueCapacity(16);
+        executor.setKeepAliveSeconds(30);
+        // 线程名称前缀
+        executor.setThreadNamePrefix("[TMSpider.Exec]-");
+        // 等待时间
+        executor.setAwaitTerminationSeconds(90);
+        // 等待所有线程执行完
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean(destroyMethod = "destroy")
+    public ThreadPoolTaskScheduler tmTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(4);
+        scheduler.setThreadNamePrefix("[TMSpider.Task]-");
+        scheduler.setAwaitTerminationSeconds(90);
+        scheduler.setWaitForTasksToCompleteOnShutdown(true);
+        scheduler.initialize();
+        return scheduler;
     }
 
     /*@Override
---
+++ tmspider/src/main/java/com/azureip/tmspider/job/TestJobs.java
@@ -0,0 +1,32 @@
+package com.azureip.tmspider.job;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestJobs {
+    private static final Logger LOG = LogManager.getLogger(TestJobs.class);
+
+    @Async("tmTaskScheduler")
+    // @Scheduled(cron = "0/5 * * * * ?")
+    public void job1() {
+        LOG.warn(Thread.currentThread().getName() + " -> Job1: started...");
+        final long start = System.currentTimeMillis();
+        while ((System.currentTimeMillis() - start) < 4000) {
+        }
+        LOG.warn(Thread.currentThread().getName() + " -> Job1: ended!");
+    }
+
+    @Async("tmTaskScheduler")
+    // @Scheduled(cron = "2/5 * * * * ?")
+    public void job2() {
+        LOG.warn(Thread.currentThread().getName() + " -> Job2: started...");
+        final long start = System.currentTimeMillis();
+        while ((System.currentTimeMillis() - start) < 4000) {
+        }
+        LOG.warn(Thread.currentThread().getName() + " -> Job2: ended!");
+    }
+}
